db5616acf54e2f18d524a1deebfe0e9729436dbe
Abhinandan Jain <nandanjain04@gmail.com>
2025-09-10 13:59:42 +0530

    Changed to async server connection to database

--- server/utils/postgres_database.py
+++ server/utils/postgres_database.py
@@ -1,5 +1,6 @@
-import psycopg2
-import psycopg2.extras
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+
 import os
 from dotenv import load_dotenv
 
@@ -7,25 +8,11 @@ load_dotenv()
 
 NEON_URI = os.getenv("NEON_URI", "")
 
-class PostgresDB:
-	def __init__(self, conn):
-		self.conn = conn
+engine = create_async_engine(NEON_URI, echo=True)
 
-	def fetch(self, query, params=None):
-		with self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-			cur.execute(query, params or ())
-			return cur.fetchall()
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+Base = declarative_base()
 
-	def fetch_one(self, query, params=None):
-		with self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-			cur.execute(query, params or ())
-			return cur.fetchone()
-
-	def execute(self, query, params=None):
-		with self.conn.cursor() as cur:
-			cur.execute(query, params or ())
-			self.conn.commit()
-
-def get_postgres_database():
-	conn = psycopg2.connect(NEON_URI, sslmode="require")
-	return PostgresDB(conn)
+async def get_db():
+	async with SessionLocal() as session:
+		yield session
